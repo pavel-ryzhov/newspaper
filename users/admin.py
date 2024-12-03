b949a02e3c29372436947eccf6b664ede6490e5d
pavel-ryzhov <pavelryzhov06@gmail.com>
2024-12-03 21:04:34 +0300

    initial commit

---
+++ users/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from users.forms import CustomUserCreationForm, CustomUserChangeForm
+from users.models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    list_display = ["username"]
+    model = CustomUser
+
+admin.site.register(CustomUser, CustomUserAdmin)
